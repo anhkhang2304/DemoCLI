e80ebed74d38ea9453a30cc4d8435a597c908e8f
KHANG\KhangPC <anhkhang2304@gmail.com>
2018-01-10 22:32:05 +0700

    services

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
@@ -8,16 +8,20 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
